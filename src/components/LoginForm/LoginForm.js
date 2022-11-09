d70fab3dd4b4b0d99366f888d057fac8f6f99460
GregoryFontana <gregjfontana@gmail.com>
2022-11-09 14:03:03 -0500

    trudging along

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,18 +1,17 @@
-// LoginForm.jsx
-
 import { useState } from "react";
 import * as usersService from "../../utilities/users-service";
+import styles from '../LoginForm/LoginForm.module.css'
 
 export default function LoginForm({ setUser }) {
   const [credentials, setCredentials] = useState({
-    email: "",
-    password: "",
+    email: " ",
+    password: " ",
   });
   const [error, setError] = useState("");
 
   function handleChange(evt) {
     setCredentials({ ...credentials, [evt.target.name]: evt.target.value });
-    setError("");
+    setError(" ");
   }
 
   async function handleSubmit(evt) {
@@ -31,28 +30,16 @@ export default function LoginForm({ setUser }) {
 
   return (
     <div>
-      <div className="form-container" onSubmit={handleSubmit}>
-        <form autoComplete="off">
-          <label>Email</label>
-          <input
-            type="text"
-            name="email"
-            value={credentials.email}
-            onChange={handleChange}
-            required
-          />
-          <label>Password</label>
-          <input
-            type="password"
-            name="password"
-            value={credentials.password}
-            onChange={handleChange}
-            required
-          />
-          <button type="submit">LOG IN</button>
-        </form>
-      </div>
-      <p className="error-message">&nbsp;{error}</p>
+    <div className="form-container">
+      <form autoComplete="off" onSubmit={handleSubmit}>
+        <label>Email</label>
+        <input type="text" name="email" value={credentials.email} onChange={handleChange} required />
+        <label>Password</label>
+        <input type="password" name="password" value={credentials.password} onChange={handleChange} required />
+        <button type="submit">LOG IN</button>
+      </form>
     </div>
+    <p className="error-message">&nbsp;{error}</p>
+  </div>
   );
 }
